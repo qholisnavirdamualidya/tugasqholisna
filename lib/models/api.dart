37c6142db9a60f99f320b407def0d4d5f6e8262f
qholisnavirdamualidya <152783587+qholisnavirdamualidya@users.noreply.github.com>
2025-02-26 12:37:46 +0700

    Add files via upload

---
+++ lib/models/api.dart
@@ -0,0 +1,7 @@
+class BaseUrl {
+  static String tambah = "http://192.168.137.191/mirna/create.php";
+  static String lihat = "http://192.168.137.191/mirna/detail.php";
+  static String edit = "http://192.168.137.191/mirna/update.php";
+  static String hapus = "http://192.168.137.191/mirna/delete.php";
+  static String data = "http://192.168.137.191/mirna/list.php";
+}
